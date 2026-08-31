e6d1cd1e90706bf047853268d629264a601c59ec
KapaRajashekarReddy-Quadrant <rajashekar.reddy@quadranttechnologies.com>
2026-08-31 13:31:32 +0530

    Uncomment DeployController implementation
    Uncommented the DeployController class and its methods.

--- src/TabularModelDeployer.Api/Controllers/DeployController.cs
+++ src/TabularModelDeployer.Api/Controllers/DeployController.cs
@@ -1,3 +1,29 @@
+// using Microsoft.AspNetCore.Mvc;
+// using TabularModelDeployer.Api.Models;
+// using TabularModelDeployer.Api.Services;
+
+// namespace TabularModelDeployer.Api.Controllers;
+
+// [ApiController]
+// [Route("api/[controller]")]
+// public class DeployController : ControllerBase
+// {
+//     private readonly TabularDeploymentService _service;
+
+//     public DeployController(TabularDeploymentService service)
+//     {
+//         _service = service;
+//     }
+
+//     [HttpPost]
+//     public IActionResult Deploy([FromBody] DeploymentRequest request)
+//     {
+//         var result = _service.DeployModel(request);
+//         return Ok(result);
+//     }
+// }
+
+
 using Microsoft.AspNetCore.Mvc;
 using TabularModelDeployer.Api.Models;
 using TabularModelDeployer.Api.Services;
