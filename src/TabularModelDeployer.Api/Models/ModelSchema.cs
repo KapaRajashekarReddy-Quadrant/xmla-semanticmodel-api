08917267c8dffa57c3a33349213d019ec4f6090b
KapaRajashekarReddy-Quadrant <rajashekar.reddy@quadranttechnologies.com>
2026-08-31 13:30:53 +0530

    Update ModelSchema.cs

--- src/TabularModelDeployer.Api/Models/ModelSchema.cs
+++ src/TabularModelDeployer.Api/Models/ModelSchema.cs
@@ -1,3 +1,52 @@
+// namespace TabularModelDeployer.Api.Models;
+
+// public class DeploymentRequest
+// {
+//     public string WorkspaceName { get; set; } = string.Empty;
+//     public string LakehouseServer { get; set; } = string.Empty;
+//     public string LakehouseDatabase { get; set; } = string.Empty;
+//     public ModelSchema ModelSchema { get; set; } = new();
+// }
+
+// public class ModelSchema
+// {
+//     public string Model_Name { get; set; } = string.Empty;
+//     public List<TableConfig> Tables { get; set; } = new();
+//     public List<RelationshipConfig> Relationships { get; set; } = new();
+// }
+
+// public class TableConfig
+// {
+//     public string Name { get; set; } = string.Empty;
+//     public bool Is_Physical { get; set; }
+//     public List<ColumnConfig> Columns { get; set; } = new();
+//     public List<MeasureConfig>? Measures { get; set; }
+// }
+
+// public class ColumnConfig
+// {
+//     public string Name { get; set; } = string.Empty;
+//     public string Type { get; set; } = string.Empty;
+//     public bool? IsHidden { get; set; }
+// }
+
+// public class MeasureConfig
+// {
+//     public string Name { get; set; } = string.Empty;
+//     public string Expression { get; set; } = string.Empty;
+// }
+
+// public class RelationshipConfig
+// {
+//     public string Name { get; set; } = string.Empty;
+//     public string From_Table { get; set; } = string.Empty;
+//     public string From_Col { get; set; } = string.Empty;
+//     public string To_Table { get; set; } = string.Empty;
+//     public string To_Col { get; set; } = string.Empty;
+// }
+
+
+
 namespace TabularModelDeployer.Api.Models;
 
 public class DeploymentRequest
